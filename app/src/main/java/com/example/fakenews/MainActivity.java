2e528534a13611f05ac0c8cccfef8a6c556239aa
Farihah Kabir <nadahkabir@gmail.com>
2019-08-01 10:13:40 +0600

    background, options, verify page

--- app/src/main/java/com/example/fakenews/MainActivity.java
+++ app/src/main/java/com/example/fakenews/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.fakenews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_landing);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+        return true;
     }
 }
