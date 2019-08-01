56d9b5727fee28a5198d0919bdacfb22eed52d77
Farihah Kabir <nadahkabir@gmail.com>
2019-08-01 10:42:42 +0600

    hopefully goes to verify page when you clock verify news

--- app/src/main/java/com/example/fakenews/MainActivity.java
+++ app/src/main/java/com/example/fakenews/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.fakenews;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +22,16 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.verify:
+                Intent intent = new Intent(this, VerifyActivity.class);
+                startActivity(intent);
+                return true;
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/fakenews/VerifyActivity.java
@@ -0,0 +1,19 @@
+package com.example.fakenews;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class VerifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
