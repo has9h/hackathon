f6dca20c8d894d518dc65b26edbd4d15e2632da2
Farihah Kabir <nadahkabir@gmail.com>
2019-08-01 11:04:16 +0600

    fakeheadlinedetector java class added

---
+++ app/src/main/java/com/example/fakenews/NewsActivity.java
@@ -0,0 +1,46 @@
+package com.example.fakenews;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.verify:
+                intent = new Intent(this, VerifyActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.news:
+                intent = new Intent(this, NewsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
